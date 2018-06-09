14c2e7ec3338f25dbca70d948e984b636cc4a288
Kuzon Chen <kuzoncby@gmail.com>
2018-06-09 12:54:43 +0800

    :snake: add docxtpl

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 **WIP**
 
 Render docx with jinja2 and send to specific email.
+
+Thanks to [docxtpl](https://github.com/elapouya/python-docx-template)
--- reports/application.py
+++ reports/application.py
@@ -1,3 +1,4 @@
+from docxtpl import DocxTemplate
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,7 +6,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    generate_report()
     return 'Hello World!'
+
+
+def generate_report():
+    doc = DocxTemplate("reports/templates/report-template.docx")
+    context = {
+        'date_start': "World company",
+        'date_end': 'End',
+        'a_reports': ['任务1', '任务2', '任务3']
+    }
+    doc.render(context)
+    doc.save('reports/templates/各研发小组20180603-20180609周工作报告.docx')
 
 
 if __name__ == '__main__':
